9788b2bc749ae94b9c40dbf7fd6aa0f73a907eaa
GISELE RODRIGUES DOS SANTOS <25203682@CFP127ALUNO.LOCAL>
2026-04-07 14:01:27 -0300

    atividade finalizada

--- app.js
+++ app.js
@@ -2,3 +2,27 @@
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
 
+function criarCard(funcionario){
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const foto = document.createElement('img')
+    foto.src = `./img/${funcionario.imagem}`
+    foto.alt = `Foto de ${funcionario.nome}`
+
+    const nome = document.createElement('h3')
+    nome.textContent = funcionario.nome
+
+    const cargo = document.createElement('span')
+    cargo.textContent = funcionario.cargo
+
+    card.append(foto, nome, cargo)
+
+    return card
+}
+
+//o Map retorna algo, diferente do forEach
+const cards = funcionarios.map(criarCard)
+
+//os três pontos espalham os elementos de um array
+document.getElementById('container').replaceChildren(...cards)
